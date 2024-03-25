319e5516bda6de3338c14d1c6c58e0d13383f6d8
Jens <jentie@users.noreply.github.com>
2024-03-25 08:49:42 +0100

    Update nano_4digit_ser.ino
    show different display modes in display

--- nano_4digit_ser/nano_4digit_ser.ino
+++ nano_4digit_ser/nano_4digit_ser.ino
@@ -16,7 +16,7 @@
   todo:
   * display analog value, esp. loop
   * set brightness?
-  * set bounds or display ranges
+  * special formats, e.g. float (-9.99 ... 9.99), time (0:00...23:59), temp (-99.9°...99.9°) 
 
 */
 
@@ -40,10 +40,14 @@ unsigned long previousMillis;  // variable for comparing millis counter
 char command[16];  // command line
 byte cptr = 0;
 
+long lbound = 0;
+long hbound = 9999;
+bool hex = false;
+
 
 bool echo = false;
-// bool debug = false;  // note: debug messages only via USB
-bool debug = true;
+bool debug = false;  // note: debug messages only via USB
+// bool debug = true;
 
 SoftwareSerial mySerial = SoftwareSerial(rxPin, txPin);  // add. serial i/f
 
@@ -54,38 +58,46 @@ const uint8_t blank[] = { 0x00, 0x00, 0x00, 0x00 };
 const uint8_t upper[] = { 0x01, 0x01, 0x01, 0x01 };
 const uint8_t lower[] = { 0x08, 0x08, 0x08, 0x08 };
 
+
 void (*resetFunc)(void) = 0;  //declare reset function @ address 0
 
 
 void test(int dtime) {
 
+  display.setSegments(blank);
+  delay(dtime);
+  display.setSegments(full);
+  delay(dtime);
+  display.setSegments(blank);
+}
+
+
+void pattern() {
+
   uint8_t data[4];
 
-  for (int n = 0; n < 3; n++) {
+  display.setSegments(upper);
+  delay(200);
+
+  for (int n = 0; n < 5; n++) {
     byte s = 1;
-    for (int m = 0; m < 8; m++) {
+    for (int m = 0; m < 6; m++) {
       data[0] = s;
       data[1] = s;
       data[2] = s;
       data[3] = s;
       display.setSegments(data);
-      delay(50);
+      delay(20);
       s = s << 1;
     }
   }
   display.setSegments(blank);
-
-  // display.setSegments(blank);
-  // delay(dtime);
-  // display.setSegments(data);
-  // delay(dtime);
-  // display.setSegments(blank);
 }
 
 
 void analogIn() {
 
-  int value = analogRead(A_IN);
+  long value = analogRead(A_IN);
 
   Serial.print(value);
   Serial.print(" - 0x");
@@ -170,7 +182,7 @@ void setup() {
 
   pinMode(CLK, OUTPUT);
   pinMode(DIO, OUTPUT);
-  display.setBrightness(5);   // 0...7
+  display.setBrightness(5);  // 0...7
 
   test(250);
 
@@ -255,7 +267,11 @@ void processCommand() {
   bool err = false;
 
   if (strcmp(command, "test") == 0) {
-    test(1000);
+    test(250);
+  } else if (strcmp(command, "pattern") == 0) {
+    pattern();
+  } else if (strcmp(command, "clear") == 0) {
+    display.setSegments(blank);
   } else if (strcmp(command, "echo") == 0) {
     echo = !echo;
   } else if (strcmp(command, "debug") == 0) {
@@ -270,6 +286,10 @@ void processCommand() {
     Serial.println(debug);
     Serial.println("commands: test, echo, debug, info, reset");
     Serial.println("analog: out <num> (0...1023), in, loop (stop with 'q')");
+    Serial.print("numbers: int, dec, hex - current range: ");
+    Serial.print(lbound);
+    Serial.print(" ... ");
+    Serial.println(hbound);
 
     mySerial.println("nano-4digit-ser - 4 digits with serial interface");
     mySerial.print("version 2024034   ");
@@ -279,6 +299,10 @@ void processCommand() {
     mySerial.println(debug);
     mySerial.println("commands: test, echo, debug, info, reset");
     mySerial.println("analog: out <num> (0...1023), in, loop (stop with 'q')");
+    mySerial.print("numbers: int, dec, hex - current range: ");
+    mySerial.print(lbound);
+    mySerial.print(" ... ");
+    mySerial.println(hbound);
 
   } else if (strncmp(command, "out ", 4) == 0) {
     analogOut();
@@ -286,48 +310,103 @@ void processCommand() {
     analogIn();
   } else if (strcmp(command, "loop") == 0) {
     analogLoop();
+
+  } else if ((command[0] == 'b') && (command[2] == 0)) {
+    int num = command[1] - '0';
+    if (num < 0)
+      num = 0;
+    else if (num > 7)
+      num = 7;
+    display.setBrightness(num);  // applied with next output
+  } else if (strcmp(command, "int") == 0) {
+    lbound = 0;
+    hbound = 9999;
+    hex = false;
+    display.showNumberDec(0, false);  // number zero
+  } else if (strcmp(command, "dec") == 0) {
+    lbound = -999;
+    hbound = 999;
+    hex = false;
+    display.showNumberDec(0, false);  // number zero
+  } else if (strcmp(command, "hex") == 0) {
+    lbound = 0x0000;
+    hbound = 0xffff;
+    hex = true;
+    display.showNumberHexEx(0x0000, 0, true);  // hex with trailing zeros
   } else {
+    // if not command --> value
 
-    int value = 0;
-    bool neg = false;
+    long value = 0;
 
-    if (command[0] == '-') {
-      neg = true;
-      command[0] = '0';
+    if (hex) {
+      int n, m;  // positions of hex digits in string
+      if ((command[0] == '0') && (command[1] == 'x'))
+        n = 2;
+      else
+        n = 0;
+      m = n + 3;  // max. 4 digits
+
+      while (true) {
+        if (command[n] == 0)
+          break;
+        value = value << 4;
+
+        int digit = command[n];
+        if (digit >= 'a')
+          digit = digit - 'a' + 10;  // string is in lower case
+        else
+          digit = digit - '0';
+
+        if ((digit < 0) || (digit > 15)) {
+          err = true;
+          value = -1;  // todo: show error pattern
+          break;
+        }
+
+        value = value + digit;
+
+        n++;
+        if (n > m)
+          break;
+      }
+
+      if (value < lbound) {
+        display.setSegments(lower);
+        err = true;
+      } else if (value > hbound) {
+        display.setSegments(upper);
+        err = true;
+      } else
+        display.showNumberHexEx(value, 0, true);  // trailing zeros
+
+    } else {
+
+      String number = command;
+
+      value = number.toInt();
+
+      if (value < lbound) {
+        display.setSegments(lower);
+        err = true;
+      } else if (value > hbound) {
+        display.setSegments(upper);
+        err = true;
+      } else
+        display.showNumberDec(value, false);
     }
 
-    String number = command;
-
     if (debug) {
-      Serial.print("value: '");
-      Serial.print(number);
-    }
-
-    value = number.toInt();
-
-    if (debug) {
-      Serial.print("' - ");
+      Serial.print(command);
+      Serial.print(" --> ");
       Serial.println(value);
     }
 
-    if (neg)
-      value = value * -1;
-
-    // if (value < 0) {
-    //   display.setSegments(lower);
-    //   err = true;
-    // } else if (value > 9999) {
-    //   display.setSegments(upper);
-    //   err = true;
-    // } else
-    display.showNumberDec(value, false);
-  }
-
-  if (err) {
-    Serial.println("err");
-    mySerial.println("err");
-  } else {
-    Serial.println("ok");
-    mySerial.println("ok");
+    if (err) {
+      Serial.println("err");
+      mySerial.println("err");
+    } else {
+      Serial.println("ok");
+      mySerial.println("ok");
+    }
   }
 }
